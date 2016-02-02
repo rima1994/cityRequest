6b9ac6d9277ac4d2e3e7db2f40c89fa7f903dc72
Louis <louisr.daviaud@gmail.com>
2016-02-02 15:59:24 +0100

    Update cityAutocomplete.js

--- cityAutocomplete.js
+++ cityAutocomplete.js
@@ -1,14 +1,41 @@
-function affichevilles(p_villes) {
-	var liste_villes = document.querySelector("#myac");
-	liste_villes.innerHTML = "";
-	for (var i = 0; i < p_villes.length; i++) {
-		var tamp = document.createElement("p");
-		tamp.textContent = p_villes[i];
-		liste_villes.appendChild(tamp);
+var villes = {
+	liste_villes : document.querySelector("#myac"),
+	reponse : "",
+	afficheVilles : function() {
+		this.liste_villes.innerHTML = "";
+		for (var i = 0; i < this.reponse.length; i++) {
+			var tamp = document.createElement("p");
+			tamp.textContent = this.reponse[i].name;
+			this.liste_villes.appendChild(tamp);
+		}
+		console.log("Fin affichevilles");
 	}
-	console.log("Fin affichevilles");
 }
-
-var toto = [ "NewYork", "Tokyo", "Paris" ];
-affichevilles(toto);
-var a = $villes;
+var yolo = [ "Tokyo", "Paris", "Londres" ];
+function callBack(httpRequest) {
+	villes.reponse = JSON.parse(httpRequest.response);
+	console.log(httpRequest);
+}
+function myajax(url, callback) {
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.open("GET", url, true);
+	httpRequest.addEventListener("load", function() {
+		callBack(httpRequest);
+	});
+	httpRequest.send(null);
+}
+function cityRequest(nom_ville) {
+	myajax("http://infolimon.iutmontp.univ-montp2.fr/~daviaudl/TD3/cityRequest.php?name="
+			+ nom_ville);
+	villes.afficheVilles();
+}
+var clavier = "";
+document.onkeydown = function(event) {
+	console.log(event.key);
+	if (event.key = "Backspace") {
+		clavier = clavier.substring(0, clavier.length - 1);
+	} else {
+		clavier += event.key;
+	}
+	cityRequest(clavier);
+}
