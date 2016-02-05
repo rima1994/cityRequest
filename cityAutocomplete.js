26f7496f9319d910ed3301514f16bff4397bd8ba
Louis <louisr.daviaud@gmail.com>
2016-02-05 03:27:28 +0100

    Update cityAutocomplete.js

--- cityAutocomplete.js
+++ cityAutocomplete.js
@@ -1,6 +1,5 @@
 var villes = {
 	liste_villes : document.querySelector("#myac"),
-	valeur_saisie = document.querySelector("#acdiv"),
 	reponse : "",
 	afficheVilles : function() {
 		this.liste_villes.innerHTML = "";
@@ -17,13 +16,15 @@ function callBack(httpRequest) {
 	villes.reponse = JSON.parse(httpRequest.response);
 	console.log(httpRequest);
 }
-function myajax(url, callback) {
+function myajax(url, callback, startLoadingAction, endLoadingAction) {
+	startLoadingAction;
 	var httpRequest = new XMLHttpRequest();
-	httpRequest.open("GET", url, true);
+	httpRequest.open("GET", url, true);*
 	httpRequest.addEventListener("load", function() {
 		callBack(httpRequest);
 	});
 	httpRequest.send(null);
+	endLoadingAction;
 }
 function cityRequest(nom_ville) {
 	myajax("http://infolimon.iutmontp.univ-montp2.fr/~daviaudl/TD3/cityRequest.php?name="
@@ -32,11 +33,16 @@ function cityRequest(nom_ville) {
 }
 var clavier = "";
 document.onkeydown = function(event) {
-	console.log(event.key);
-	setTimeout(cityRequest(clavier), 200));
+	clavier = document.querySelector("#acdiv").querySelector("input").value;
+	debounce();
 }
-var bouton = villes.valeur_saisie;
-bouton.addEventListener("click", function(event) {
-	clavier = event.target;
-	game.fiftyfifty(event);
-})
+function debounce() {
+	console.log("debounce");
+	window.setTimeout(cityRequest(clavier), 200);
+}
+function startLoadingAction() {
+
+}
+function endLoadingAction() {
+
+}
